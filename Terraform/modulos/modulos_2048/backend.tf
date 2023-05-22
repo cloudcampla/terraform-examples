eb7a842ba42390637478d00c12ff839a5b867637
sharwin <idbellog@gmail.com>
2023-05-22 16:19:27 -0500

    changing name to 2048

---
+++ Terraform/modulos/modulos_2048/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "cloudcamp-terraform-state"
+    key    = "modulo_wordpress/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "cloudcamp-ddb-lock"
+    encrypt = true
+  }
+}
