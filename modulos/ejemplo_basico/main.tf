b908b095a6af1a0024725fb3fb07c5931c9dbd74
BELLI010 <idbellog@gmail.com>
2023-05-24 17:03:49 -0500

    updating ejemplo basico

--- modulos/ejemplo_basico/main.tf
+++ modulos/ejemplo_basico/main.tf
@@ -1,5 +1,12 @@
 // main.tf
 module "example_module" {
-  source       = "mi_modulo"
-  instance_type = "t2.small"
+  source       = "./mi_modulo"
+}
+
+output "ec2_id" {
+  value = module.example_module.instance_id
+}
+
+output "ec2_ip" {
+  value = module.example_module.ip
 }
