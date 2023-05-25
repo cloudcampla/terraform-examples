8f2f0e74f74dfeabc7b3690e42189dd46a196b2c
BELLI010 <idbellog@gmail.com>
2023-05-24 21:59:52 -0500

    adding_class_changes

--- modulos/modulo_2048_2/main.tf
+++ modulos/modulo_2048_2/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
 }
 
 module "ec2" {
-  source = "git::https://github.com/Sharwin/ec2.git?ref=main"
+  source = "git::https://github.com/Sharwin/ec2.git?ref=v1.0.0"
   vpc_id  = module.vpc.vpc_id
   subnet_id = module.vpc.public_subnet_id
   key_pair_name = "ivan.bello"
