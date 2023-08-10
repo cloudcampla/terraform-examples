c22e204dda945eac60d64f65df42f6c9910bb8e9
BELLI010 <ivan.bello.-nd@disney.com>
2023-08-10 17:58:22 -0500

    adding conceptos iniciales files

---
+++ conceptos/taint_new/taint.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "amzn-linux-2023-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+resource "aws_instance" "cloudcamp" {
+  ami           = data.aws_ami.amzn-linux-2023-ami.id
+  instance_type = "t2.micro"
+
+  tags = {
+    bootcamp = "devops"
+  }
+}
